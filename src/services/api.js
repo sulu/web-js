255d63f6cb4044148e19bf59232524cc286e4b88
Alexander Schranz <alexander.schranz@massiveart.com>
2018-02-24 00:56:14 +0100

    add service to communicate with apis

---
+++ src/services/api.js
@@ -0,0 +1,47 @@
+// Api Service to communication with api
+
+'use strict';
+
+var $ = require('jquery');
+
+var Api = (function() {
+    var api = {};
+
+    /**
+     * @method ajax
+     * @param {String} method
+     * @param {String} uri
+     * @param {Object} data
+     * @private
+     * @returns {promise}
+     */
+    var ajax = function(method, uri, data) {
+        return $.ajax({
+            url: uri,
+            method: method,
+            data: data
+        });
+    };
+
+    // -------- HTTP VERBS ----------
+    api.get = function(uri, data) {
+        return ajax('get', uri, data);
+    };
+
+    api.post = function(uri, data) {
+        return ajax('post', uri, data);
+    };
+
+    api.put = function(uri, data) {
+        return ajax('put', uri, data);
+    };
+
+    api.delete = function(uri, data) {
+        return ajax('delete', uri, data);
+    };
+
+    return api;
+})();
+
+module.exports = Api;
+
