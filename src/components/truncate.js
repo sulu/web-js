5f467311594e6d8b96910b8cc470bfcf9d8107d2
Luca Rath-Heel <luca-rath@hotmail.com>
2018-11-02 17:23:42 +0100

    Update truncate.js

--- src/components/truncate.js
+++ src/components/truncate.js
@@ -10,20 +10,20 @@ var $ = require('jquery');
  */
 function debounce(func, wait, immediate) {
     var timeout;
-    
+
     return function() {
         var context = this;
         var args = arguments;
-        
+
         clearTimeout(timeout);
         timeout = setTimeout(function() {
             timeout = null;
-            
+
             if (!immediate) {
                 func.apply(context, args);
             }
         }, wait);
-        
+
         if (immediate && !timeout) {
             func.apply(context, args);
         }
